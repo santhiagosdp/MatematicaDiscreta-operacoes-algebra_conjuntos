2f513f0f869984b1e3c7fbf3b9cdf635f1e35f34
marcusmarcelino <marcusmarcelinosilva1010@gmail.com>
2019-03-13 15:20:05 -0300

    Update algoritmos.py
    Adição do algoritmo de diferença dos conjuntos

--- algoritmos.py
+++ algoritmos.py
@@ -34,3 +34,23 @@ def interseccao(A,B):
   return I
 #Fim-Intersecção
 print('Intersecção',interseccao([1,2,3,4],[4,6,7,1,9,0]))
+
+#Diferença dos conjuntos
+def diferenca(conjunto1,conjunto2):
+  Dif = []
+  U = uniao(conjunto1,conjunto2)
+
+  for elem_U in U:
+    for elem_conj1 in conjunto1:
+      verifica=0
+      if (elem_conj1 == elem_U):
+        for elem_conj2 in conjunto2:
+          if (elem_U == elem_conj2):
+            verifica=1
+            break
+        if(verifica==0):
+          Dif.append(elem_U)
+          
+  return Dif
+#Fim-Diferença dos Conjuntos
+print('Diferença dos conjuntos',diferenca([1,2,3,4,6],[1,4,6,7,9,0]))
